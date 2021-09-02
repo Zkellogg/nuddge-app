501e4f0c7cdf47097c09dae8c5a19fa429bac479
Jen Ine <jbscott_5@yahoo.com>
2021-09-02 16:13:36 -0400

    update

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express()
 
 const session = require('express-session')
 
+const hostname = '127.0.0.1';
 
-
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 
 const mustacheExpress = require('mustache-express');
 
@@ -80,6 +80,6 @@ app.post('/login', (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://${port}/`);
+app.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 })
